09435ec9a9d0d8d3b30a449ac757f919c6c8d54b
Kostas <kkarasavvas@gmail.com>
2026-06-01 14:21:20 +0300

    4 new time for slide 136

--- data.js
+++ data.js
@@ -1265,7 +1265,7 @@ window.storyScenes = [
     {
         "slideNumber": 136,
         "audioSrc": "assets/audio/chapter-2.m4a",
-        "startTime": 103.0,
+        "startTime": 102.2,
         "image": "assets/images/soon.svg",
         "text": "So great was his worth that the Bear People—or Pelwe, as some named them—bestowed upon him the Spirit of the Bear, a relic of their tribe, an axe of mighty craft given only to a champion proven in valor.",
         "chapterId": "chapter-2"
